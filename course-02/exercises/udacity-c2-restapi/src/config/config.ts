da062611ee8be2f63e07d87474c43e76b8027716
almir <almir.omerovic.dev@gmail.com>
2021-07-11 17:25:50 +0200

    prepare for cloud

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -9,7 +9,7 @@ export const config = {
         dialect: 'postgres',
         aws_region: process.env.AWS_REGION,
         aws_profile: process.env.AWS_PROFILE,
-        aws_media_bucket: process.env.aws_media_bucket,
+        aws_media_bucket: process.env.AWS_MEDIA_BUCKET,
     },
     prod: {
         username: '',
